6699c0ac1f55e02a461284b991337305e124dcb0
siscodr <siscodr@rose-hulman.edu>
2015-03-20 21:29:32 -0400

    AddedTestCases
    Hotel's Test Cases added from Lab Instructions
    And
    Flight's Test Cases are self implemented to test the Flight Code.

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -7,6 +7,46 @@ namespace ExpediaTest
 	[TestFixture()]
 	public class FlightTest
 	{
-		//TODO Task 7 items go here
+        private readonly int MilesToFly = 5;
+
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            var target = new Flight(new DateTime(2009, 11, 1), new DateTime(2009, 11, 30), 5);
+            Assert.IsNotNull(target);
+        }
+
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForOneDayStay()
+        {
+            var target = new Flight(new DateTime(2009, 11, 1), new DateTime(2009, 11, 2), MilesToFly);
+            Assert.AreEqual(220, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForTwoDayStay()
+        {
+            var target = new Flight(new DateTime(2009, 11, 1), new DateTime(2009, 11, 3), MilesToFly);
+            Assert.AreEqual(240, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForTenDaysStay()
+        {
+            var target = new Flight(new DateTime(2009, 11, 1), new DateTime(2009, 11, 11), MilesToFly);
+            Assert.AreEqual(400, target.getBasePrice());
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatFlightThrowsOnBadLength()
+        {
+            new Flight(new DateTime(2009, 11, 1), new DateTime(2009, 11, 3), -5);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestThatFlightThrowsOnBadDates()
+        {
+            new Flight(new DateTime(2009, 11, 30), new DateTime(2009, 11, 1), 5);
+        }
 	}
 }
